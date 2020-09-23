df35eb499f5e429de68fec5711ba4365908197a8
sobhansahu <62336624+sobhansahu@users.noreply.github.com>
2020-09-23 15:23:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,21 +136,7 @@ resource "aws_eks_fargate_profile" "example" {
   }
 }
 
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/templates/kubeconfig.tpl")
 
-  vars = {
-    kubeconfig_name           = "eks_${aws_eks_cluster.main.name}"
-    clustername               = aws_eks_cluster.main.name
-    endpoint                  = data.aws_eks_cluster.cluster.endpoint
-    cluster_auth_base64       = data.aws_eks_cluster.cluster.certificate_authority[0].data
-  }
-}
-
-resource "local_file" "kubeconfig" {
-  content  = data.template_file.kubeconfig.rendered
-  filename = pathexpand("${var.kubeconfig_path}/config")
-}
 
 output "cluster_id" {
   description = "ID of the created cluster"
