90564287bf765705dba2e694587a3e8eda16e4ac
sobhansahu <62336624+sobhansahu@users.noreply.github.com>
2020-09-23 11:39:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -150,11 +150,6 @@ resource "local_file" "kubeconfig" {
   filename = pathexpand("${var.kubeconfig_path}/config")
 }
 
-output "kubectl_config" {
-  description = "Path to new kubectl config file"
-  value       = pathexpand("${var.kubeconfig_path}/config")
-}
-
 output "cluster_id" {
   description = "ID of the created cluster"
   value       = aws_eks_cluster.main.id
