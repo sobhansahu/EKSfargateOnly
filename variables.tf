ca372daa0ad2698a294fb97596774365a21b28a0
sobhansahu <62336624+sobhansahu@users.noreply.github.com>
2020-09-23 14:20:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
 variable "name" {
   description = "the name of your stack, e.g. \"demo\""
+  type = string
+  default = "testcluster"
 }
 
 variable "environment" {
   description = "the name of your environment, e.g. \"prod\""
+  type = string
+  default = "infosyssubscription"
 }
 
 variable "region" {
@@ -18,12 +22,17 @@ variable "vpc_id" {
   description = "The VPC the cluser should be created in"
 }
 
-variable "private_subnets" {
+variable "public_subnets" {
+  type    = list
+  default = ["training-vpc-public-us-east-2a", "training-vpc-public-us-east-2b", "training-vpc-public-us-east-2c"]
   description = "List of private subnet IDs"
+  
 }
 
-variable "public_subnets" {
+variable "private_subnets" {
   description = "List of private subnet IDs"
+  type    = list
+  default = ["training-vpc-public-us-east-2a", "training-vpc-public-us-east-2b", "training-vpc-public-us-east-2c"]
 }
 
 variable "kubeconfig_path" {
